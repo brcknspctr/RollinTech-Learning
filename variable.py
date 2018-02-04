f664b23375606fc81300fae0952b02ccb5b826e7
小熊麦 <xiaoxiongmai@ogumamugi-no-MacBook-Air.local>
2018-02-05 01:06:00 +0900

    testing

--- variable.py
+++ variable.py
@@ -1,3 +1,18 @@
-a = 100
-b = 346
-print(a*b)
+def total(a, b):
+    if a + b > 100:
+        return 100
+    else:
+        return a + b
+
+def keepup(a, b):
+    if a + b < target:
+        return target
+    else:
+        return a + b
+
+x = 10
+y = 20
+
+print(total(x,y))
+target = total(x,y)
+print(keepup(30,5))
